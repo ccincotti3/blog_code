94f2cf1de51a9f3338120a204628c74b8ccc1c60
Carmen Cincotti <ccincotti3@gmail.com>
2022-03-28 08:37:59 -0400

    Add explicit, and editor config.

--- 2022-04-04/main.c
+++ 2022-04-04/main.c
@@ -9,40 +9,55 @@
 #include <stdio.h>
 #include "matrix.h"
 
-int main() {
+int main(int argc, char *argv[]) {
+    // Open files to write data to
+    FILE* fp = NULL;
+    fp = fopen("result.txt", "w");
+
     float x0 = 100000.0;
     float y0 = 100.0;
 
-    float h = 1;
+    float h = 2;
     int maxSteps = 10;
 
     float k = 1.0;
 
-    // Xnew = X0 + hf(Xnew)
+    // Xnew = X0 + hTerm*dX
     Matrix Xnew = MatCreate(2, 1);
-    Matrix X0 = MatCreate(2, 1);
+    Matrix hTerm = MatCreate(2, 2);
+    Matrix dX = MatCreate(2, 1);
+
     MatSet(&Xnew, 0, 0, x0);
     MatSet(&Xnew, 1, 0, y0);
 
-    // X0 + dX = X0 + hf(X0 + dX).
-    Matrix dX = MatCreate(2, 2);
-    MatSet(&dX, 0, 0, -h / (h + 1.0));
-    MatSet(&dX, 1, 1, -h / (k*h + 1.0));
+    // MatSet(&hTerm, 0, 0, -h / (h + 1.0));
+    // MatSet(&hTerm, 1, 1, -h / (k*h + 1.0));
 
-    Matrix TempDeltaX = MatCreate(2,2);
+    MatSet(&hTerm, 0, 0, -h);
+    MatSet(&hTerm, 1, 1, -h);
 
-    for(int i = 0; i < maxSteps;++i) {
-        MatPrint(&Xnew);
-        MatSet(&X0, 0, 0, MatGet(&Xnew, 0, 0));
-        MatSet(&X0, 1, 0, MatGet(&Xnew, 1, 0) * k);
-        TempDeltaX = MatMultiply(&dX, &X0);
+    Matrix TempDeltaX = MatCreate(2, 2);
+
+    for (int i = 0; i < maxSteps;++i) {
+        fprintf(fp, "%f\t %f\n", h * i, y0);
+
+        MatSet(&dX, 0, 0, x0);
+        MatSet(&dX, 1, 0, y0 * k);
+
+        TempDeltaX = MatMultiply(&hTerm, &dX);
+
+        // Xnew = X0 + DeltaX
         MatAdd(&Xnew, &Xnew, &TempDeltaX);
+
+        x0 = MatGet(&Xnew, 0, 0);
+        y0 = MatGet(&Xnew, 1, 0);
     }
 
     MatPrint(&Xnew);
-   
+
     // Clean up
-    MatDestroy(&X0);
+    MatDestroy(&Xnew);
+    MatDestroy(&hTerm);
     MatDestroy(&dX);
 
     return 0;
